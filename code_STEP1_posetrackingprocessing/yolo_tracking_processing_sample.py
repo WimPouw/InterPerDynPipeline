80c57ef07eb74b02f8d7d398c489117aa24f3a73
Wim Pouw <wimpouw@gmail.com>
2025-01-01 18:24:52 +0100

    update

--- code_STEP1_posetrackingprocessing/yolo_tracking_processing_sample.py
+++ code_STEP1_posetrackingprocessing/yolo_tracking_processing_sample.py
@@ -8,12 +8,14 @@ import os
 import torch # for gpu support
 
 torch.cuda.set_device(0)
-
+# Load the model
+model = YOLO('yolov8x-pose-p6.pt') # heaviest model
 # main variables
-video_folder = "./data_sample_data_raw/"
-video_files = glob.glob(video_folder + "*.mp4")
-step1resultfolder = "./data_sample_data_raw/step1result/" # we can replace with full data
-
+video_folder = "../data_sampledata_raw/"
+# avi mp4 or other video formats
+video_files = glob.glob(video_folder + "*.mp4") + glob.glob(video_folder + "*.avi")
+step1resultfolder = "../step1result/" # we can replace with full data
+print(video_files)
 
 class GetKeypoint(BaseModel):
     NOSE:           int = 0
@@ -52,12 +54,12 @@ skeleton = [
     (get_keypoint.RIGHT_KNEE, get_keypoint.RIGHT_ANKLE),
 ]
 
-# Load the model
-model = YOLO('yolov8x-pose-p6.pt')
-
 for video_path in video_files:
     # Video path
     video_path = video_path
+    # vidname without extension
+    vidname = os.path.basename(video_path)
+    vidname = vidname.split('.')[0]
 
     # Open the video
     cap = cv2.VideoCapture(video_path)
@@ -69,12 +71,12 @@ for video_path in video_files:
 
     # Define the output video writer
     corename = os.path.basename(video_path).split('.')[0]
-    output_path = step1resultfolder+"{corename}_output.mp4"
+    output_path = step1resultfolder+ vidname+"_annotated_layer1.mp4"
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
 
     # Prepare CSV file
-    csv_path = 'keypoints_data.csv'
+    csv_path = step1resultfolder+ vidname+'_keypoints_data_layer1.csv'
     csv_file = open(csv_path, 'w', newline='')
     csv_writer = csv.writer(csv_file)
 
@@ -104,23 +106,21 @@ for video_path in video_files:
         if len(results[0].keypoints.xy) > 0:
             # Process the results
             for person_idx, person_keypoints in enumerate(results[0].keypoints.xy):
-                #only process the first two people
-                if person_idx < 2:
-                    for keypoint_idx, keypoint in enumerate(person_keypoints):
-                        x, y = keypoint
-                        # Write to CSV
-                        csv_writer.writerow([frame_count, person_idx, keypoint_idx, x.item(), y.item()])
-                        
-                        # Draw keypoint on the frame
-                        cv2.circle(annotated_frame, (int(x), int(y)), 5, (0, 255, 0), -1)
+                for keypoint_idx, keypoint in enumerate(person_keypoints):
+                    x, y = keypoint
+                    # Write to CSV
+                    csv_writer.writerow([frame_count, person_idx, keypoint_idx, x.item(), y.item()])
                     
-                    # Draw skeleton
-                    for connection in skeleton:
-                        if connection[0] < len(person_keypoints) and connection[1] < len(person_keypoints):
-                            start_point = tuple(map(int, person_keypoints[connection[0]]))
-                            end_point = tuple(map(int, person_keypoints[connection[1]]))
-                            if all(start_point) and all(end_point):  # Check if both points are valid
-                                cv2.line(annotated_frame, start_point, end_point, (255, 0, 0), 2)
+                    # Draw keypoint on the frame
+                    cv2.circle(annotated_frame, (int(x), int(y)), 5, (0, 255, 0), -1)
+                
+                # Draw skeleton
+                for connection in skeleton:
+                    if connection[0] < len(person_keypoints) and connection[1] < len(person_keypoints):
+                        start_point = tuple(map(int, person_keypoints[connection[0]]))
+                        end_point = tuple(map(int, person_keypoints[connection[1]]))
+                        if all(start_point) and all(end_point):  # Check if both points are valid
+                            cv2.line(annotated_frame, start_point, end_point, (255, 0, 0), 2)
         
         # Write the frame to the output video
         out.write(annotated_frame)
